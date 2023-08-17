a1944fc1c471e609155d4ae7c20dd4032a503e76
Daphne210 <dapnephinerry2@gmail.com>
2023-08-17 12:21:00 +0300

    Updates from Master

--- packages/esm-ugandaemr-app/src/types.ts
+++ packages/esm-ugandaemr-app/src/types.ts
@@ -79,18 +79,6 @@ export interface Search {
   mode: string;
 }
 
-export const spaBasePath = `${window.spaBase}/home`;
-
-export enum SearchTypes {
-  BASIC = 'basic',
-  ADVANCED = 'advanced',
-  SEARCH_RESULTS = 'search_results',
-  SCHEDULED_VISITS = 'scheduled-visits',
-  VISIT_FORM = 'visit_form',
-  QUEUE_SERVICE_FORM = 'queue_service_form',
-  QUEUE_ROOM_FORM = 'queue_room_form',
-}
-
 export interface WaitTime {
   metric: string;
   averageWaitTime: string;
